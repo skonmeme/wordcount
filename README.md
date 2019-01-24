e3061574f50a06d8e22bcb4229d65a60e20c3fd1
Sung Gon Yi <skonmeme@gmail.com>
2019-01-24 20:38:14 +0900

    Initial version of Flink WordCount

---
+++ README.md
@@ -0,0 +1,13 @@
+## WordCount on Flink
+
+###### Open socket with port 9000 
+```
+nc -l 9000
+```
+
+###### Start Flink job
+```
+flink run -c com.skt.skon.wordcount 
+```
+
+Check you taskmanager.
---
+++ src/main/scala/com/skt/skon/wordcount/WordCount.scala
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.skon.wordcount
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+case class WordWithCount(word: String, count: Int)
+
+object WordCount {
+
+  def main(args: Array[String]) {
+    // set up the streaming execution environment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val textStream = env.socketTextStream("localhost", 9000, '\n')
+
+    val wordStream = textStream
+      .flatMap { line => line.split("\\s") }
+      .map { word => WordWithCount(word, 1) }
+      .name("Split a text to words")
+
+    val wordCount = wordStream
+      .keyBy( _.word )
+      .timeWindow(Time.seconds(5))
+      .sum("count")
+
+    wordCount.print().name("Count words")
+      .setParallelism(1)
+
+    // execute program
+    env.execute("Flink Streaming Scala API Skeleton")
+  }
+
+}
